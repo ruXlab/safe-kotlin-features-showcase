fbe1a2fe20495a57f7f52c0d8a9826ee24cc5ad9
ruX <post4ruX@gmail.com>
2017-05-04 01:23:29 +0100

    Change example with named constructor arguments

--- src/named_params/KNamedArgumentInConstructor.kt
+++ src/named_params/KNamedArgumentInConstructor.kt
@@ -1,10 +1,12 @@
 package named_params
 
+import java.io.File
+
 /**
  * Created by rux on 02/05/17.
  */
 
-data class Parcel(
+class Parcel(
     val id: Long,
     val width: Double,
     val height: Double,
@@ -14,9 +16,24 @@ data class Parcel(
     val value: Double
 )
 
+class MyService(
+    val config: Map<String, String>,
+    val input: File,
+    val output: File,
+    val offsetX: Int,
+    val offsetY: Int,
+    val items: List<String>
+)
+
 fun main(args: Array<String>) {
-    val p = Parcel(542342342, weight = 10.5,
-            width = 40.0, height = 215.0, length = 40.5,
-            price = 49.99, value = 100.0)
-    println(p)
+    val items = listOf("one", "two", "three")
+
+    val service = MyService(
+        config = loadConfig(), items = items,
+        input = File("input.txt"), output = File("output.txt"),
+        offsetX = 42, offsetY = 64
+    )
+
 }
+
+fun loadConfig() = mapOf("test" to "true")
